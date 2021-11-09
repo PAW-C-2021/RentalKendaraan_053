bbdcce9c9ffe898f230be938268b3093f824ab30
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2021-11-09 16:53:25 +0700

    Add project files.

---
+++ RentalKendaraan/Models/Peminjaman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan.Models
+{
+    public partial class Peminjaman
+    {
+        public int IdPeminjaman { get; set; }
+        public DateTime? TglPeminjaman { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCustomer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+        public virtual Customer IdCustomerNavigation { get; set; }
+        public virtual Jaminan IdJaminanNavigation { get; set; }
+        public virtual Kendaraan IdKendaraanNavigation { get; set; }
+        public virtual Pengembalian IdPeminjamanNavigation { get; set; }
+    }
+}
---
+++ RentalKendaraan/Models/Pengembalian.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan.Models
+{
+    public partial class Pengembalian
+    {
+        public int IdPengambilan { get; set; }
+        public DateTime? TglPengambilan { get; set; }
+        public int? IdPeminjaman { get; set; }
+        public int? IdKondisi { get; set; }
+        public int? Denda { get; set; }
+
+        public virtual KondisiKendaraan IdKondisiNavigation { get; set; }
+        public virtual Peminjaman Peminjaman { get; set; }
+    }
+}
